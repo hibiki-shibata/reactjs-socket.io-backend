86b295d3fe45d9115815e939a0da53740fd0086a
Hibiki Shibata <hibikishibata1853@gmail.com>
2023-05-31 17:24:09 +0900

    fixed by dutch

--- index.js
+++ index.js
@@ -2,35 +2,38 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const { Socket } = require("socket.io");
-const { Server} = require("socket.io");
+const { Server } = require("socket.io");
 app.use(cors());
+
+// import databasefire from './firebase'
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors:{
-        origin: "http://localhost:3000",
-        methods: ["GET", "`POST"],
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
     },
-});
-
-const [messageList2, setMessagelist2] = useState([]);
-
-io.on("connection", (socket) =>{
-    console.log('SOCKET CONNECTED');
-    
-    socket.on("disconnect", () => {
-        console.log("SOCKET DISCONNECTED", socket.id);
-    })
-});
-
-
-socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
   });
+// const [messageList2, setMessagelist2] = useState([]);
 
+var k = 0;
 
-server.listen(3001, () => {
-    console.log(("Server running yayy!"));
-});
+io.on("connection", (socket) => {
+    console.log(`CONNECTED`);
+  
+    socket.on("send_message", (data) => {
+      console.log("send　message ",k)
+      data["message"] =  data["message"] + k
+      io.emit("receive_message", data);
+      k++;
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("DISCONNECT");
+    });
+  });
+  
+  server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+  });
